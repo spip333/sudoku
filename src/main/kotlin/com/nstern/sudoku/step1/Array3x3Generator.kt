66e5c5a4bced455cc746c600ff99150c8bfc7ee0
U115136 <U115136@mobi.ch>
2023-12-18 09:33:23 +0100

    new increment

--- src/main/kotlin/com/nstern/sudoku/step1/Array3x3Generator.kt
+++ src/main/kotlin/com/nstern/sudoku/step1/Array3x3Generator.kt
@@ -2,7 +2,29 @@ package com.nstern.sudoku.step1
 
 class Array3x3Generator {
 
-    fun generate3x3Array(): Array<Int>{
-        return emptyArray<Int>()
+    val nums = (1..9).toList()
+
+    fun generate3x3Array(): Array<IntArray>{
+        val s = nums.shuffled()
+        val num1 = s.subList(0,3).toIntArray()
+        val num2 = s.subList(3,6).toIntArray()
+        val num3 = s.subList(6,9).toIntArray()
+
+        val result = arrayOf(num1, num2, num3)
+        return result
+    }
+
+    fun generateSudokuGrid(): Array<Array<IntArray>>{
+        val result = arrayOf(
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array(),
+            generate3x3Array())
+        return result
     }
 }
--- src/test/kotlin/com/nstern/sudoku/step1/Array3x3GeneratorTest.kt
+++ src/test/kotlin/com/nstern/sudoku/step1/Array3x3GeneratorTest.kt
@@ -1,13 +1,28 @@
 package com.nstern.sudoku.step1
 
 import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
-class Array3x3GeneratorTest{
+class Array3x3GeneratorTest {
 
     val sut = Array3x3Generator()
 
     @Test
-    fun doTest(){
+    fun generate3x3ArrayAlwaysCreatesAnArrray3x3Containing9numbers() {
         val result = sut.generate3x3Array()
+        val checkResultSet: MutableSet<Int?> = mutableSetOf<Int?>()
+
+        for (i in 0..2) {
+            checkResultSet.addAll(result[i].toSet())
+        }
+        val nums = (1..9).toList()
+        assertThat(checkResultSet).containsAll(nums)
+        assertThat(checkResultSet.size).isEqualTo(9)
+    }
+
+    @Test
+    fun generateSudokuGridShouldGenerateAnArrayOf9x9() {
+        val result = sut.generateSudokuGrid()
+        assertThat(result.size).isEqualTo(9)
     }
 }
